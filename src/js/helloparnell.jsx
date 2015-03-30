6bd0871eebd3b4628d8d06cd520d45c6c40981d5
Jeremy Moritz <jeremy@jeremymoritz.com>
2015-03-30 09:52:43 -0500

    double quotes

--- src/js/helloparnell.jsx
+++ src/js/helloparnell.jsx
@@ -1,8 +1,8 @@
 React.render(
 	<div>
-		<img src='img/parnell-logo.jpg' alt className='img-responsive' />
+		<img src="img/parnell-logo.jpg" alt className="img-responsive" />
 		<h1>Hello Parnell!</h1>
-		<img src='img/parnell-footer.jpg' alt className='img-responsive' />
+		<img src="img/parnell-footer.jpg" alt className="img-responsive" />
 	</div>,
 	document.getElementById('hello-parnell')
 );
